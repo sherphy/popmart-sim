ad4ce0b80c77219dd2458aa99f56a91ebd4abce3
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-03-06 06:22:27 +0800

    popmart-simulator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>POPMART pull simulator</title>
+        <link rel="icon" href="./public/favicon.ico"/>
+      </header>
+      <body>
+        <h1> Hello </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
