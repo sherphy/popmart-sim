3304fa2515dc975558439da42e947bab2649a96c
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-03-06 08:12:03 +0800

    working for fake url

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <title>POPMART pull simulator</title>
-        <link rel="icon" href="./public/favicon.ico"/>
-      </header>
-      <body>
-        <h1> Hello </h1>
-      </body>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    //change the URL to popmart 
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state; 
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else { 
+    return (
+      <div className='App'>
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              Name: {item.name}
+            </li>
+          ))};
+        </ul>
+      </div>
+    );
+    }
+  }
 }
 
-export default App;
+export default App
