67ab2b8e5ec868ce63f520d2784cafdeeb3b1dde
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-03-15 18:31:00 +0800

    vercel trial

--- server/index.js
+++ server/index.js
@@ -4,37 +4,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const PopmartModel = require("./models/Popmart");
-const vercelToken = 'EIGjq6rrcNHnz4hJdUFxN5j3';
-const apiEndPt = 'https://api.vercel.com/v9/projects';
-const fs = require('fs');
-const axios = require('axios');
-
-
-let config = {
-  method: 'get',
-  url: apiEndPt,
-  headers: {
-    Authorization: 'Bearer ' + vercelToken,
-  },
-};
-let results = [];
- 
-(function loop() {
-  axios(config)
-    .then(function (response) {
-      results.push(...response.data.projects);
-      if (response.data.pagination.next !== null) {
-        config.url = `${apiEndPt}?until=${response.data.pagination.next}`;
-        loop();
-      } else {
-        //you can use the final results object and for example save it to a json file
-        fs.writeFileSync('projects.json', JSON.stringify(results));
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-})();
 
 //mongoose compass takes different connection url
 //take the connection from the application side 
